f808385ef73cc58407d45a2f9218e2356f50672a
yusung103 <gimyuseong742@gmail.com>
2024-10-15 20:40:38 +0900

    first commit

---
+++ src/main/java/com/example/domaserver/DoMaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.domaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoMaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoMaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/domaserver/DoMaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.domaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoMaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
