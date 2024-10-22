06d3fdbfd35d7eefa235613ede4455b2f24707df
yusung103 <gimyuseong742@gmail.com>
2024-10-22 12:07:51 +0900

    add :: ErrorCodes

--- src/main/java/com/example/domaserver/global/exception/ErrorCode.java
+++ src/main/java/com/example/domaserver/global/exception/ErrorCode.java
@@ -5,7 +5,12 @@ import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public enum ErrorCode {;
+public enum ErrorCode {
+
+    EXPIRED_TOKEN(401, "토큰이 만료되었습니다."),
+    INVALID_TOKEN_TYPE(401, "유효하지 않은 토큰 타입입니다."),
+    INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
+    EXPIRED_REFRESH_TOKEN(401, "만료된 리프레쉬 토큰입니다.");
 
     private final int httpStatus;
     private final String message;
