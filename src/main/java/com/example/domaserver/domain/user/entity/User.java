1d3661f66d6a65639904b21054c1748f78bd4d05
김한주 <s24043@gsm.hs.kr>
2024-10-24 12:18:06 +0900

    User.java

--- src/main/java/com/example/domaserver/domain/user/entity/User.java
+++ src/main/java/com/example/domaserver/domain/user/entity/User.java
@@ -1,15 +1,13 @@
 package com.example.domaserver.domain.user.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.UUID;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,9 +20,51 @@ public class User {
 
     private String email;
 
+    private String to_day_clean;
+
+    private int penalty_point;
+    private int clean_point;
+    private int rank;
+
     @Embedded
     private StudentNum studentNum;
 
     @Enumerated(EnumType.STRING)
     private Authority authority;
+
+    public User(UUID id, String name, String to_day_clean,
+                int penalty_point, int clean_point, int rank) {
+        this.id = id;
+        this.name = name;
+        this.to_day_clean = to_day_clean;
+        this.penalty_point = penalty_point;
+        this.clean_point = clean_point;
+        this.rank = rank;
+        this.studentNum = new StudentNum();
+    }
+
+    public int getPenaltyPoint() {
+        return penalty_point;
+    }
+    public void setPenaltyPoint(int penalty_point) {
+        this.penalty_point = penalty_point;
+    }
+
+    public int getCleanPoint() {
+        return clean_point;
+    }
+    public void setCleanPoint(int clean_point) {
+        this.clean_point = clean_point;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+
+
+
 }
