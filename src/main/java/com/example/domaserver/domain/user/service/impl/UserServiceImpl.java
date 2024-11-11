bc65f7af2900182c3519bd5103de7426e2e2fef5
김한주 <s24043@gsm.hs.kr>
2024-11-11 16:47:28 +0900

    add :: UserServiceImpl.java

---
+++ src/main/java/com/example/domaserver/domain/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.domaserver.domain.user.service.impl;
+
+import com.example.domaserver.domain.user.entity.User;
+import com.example.domaserver.domain.user.repository.UserRepository;
+import com.example.domaserver.domain.user.service.UserService;
+import com.example.domaserver.global.annotation.ServiceWithTransaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.UUID;
+
+@ServiceWithTransaction
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+
+    @Override
+    public User findById(UUID id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with id: " + id));
+    }
+
+}
