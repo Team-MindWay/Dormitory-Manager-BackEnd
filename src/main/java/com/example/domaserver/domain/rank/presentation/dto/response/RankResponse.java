7dce3ef63a6820b00e7d46745e062c39c149c726
김한주 <s24043@gsm.hs.kr>
2024-11-11 16:42:02 +0900

    add :: RankResponse.java

---
+++ src/main/java/com/example/domaserver/domain/rank/presentation/dto/response/RankResponse.java
@@ -0,0 +1,16 @@
+package com.example.domaserver.domain.rank.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Builder
+@Getter
+public class RankResponse {
+    private UUID Id;
+    private String name;
+    private String profileImageUrl;
+    private int penaltyPoints;
+    private double RankScore;
+}
