2fc1e70e69f48872a519b341b8b6b50f651200b2
김한주 <s24043@gsm.hs.kr>
2024-11-14 19:52:10 +0900

    add :: UpdateRankingService.java

---
+++ src/main/java/com/example/domaserver/domain/rank/service/UpdateRankingService.java
@@ -0,0 +1,7 @@
+package com.example.domaserver.domain.rank.service;
+
+import com.example.domaserver.domain.rank.entity.Rank;
+
+public interface UpdateRankingService {
+    void updateRanking(Rank rank);
+}
