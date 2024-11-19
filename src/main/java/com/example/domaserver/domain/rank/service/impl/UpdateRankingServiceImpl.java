75819788f58be02726a594146473e6899646dec0
김한주 <s24043@gsm.hs.kr>
2024-11-19 20:01:31 +0900

    modify :: UpdateRankingServiceImpl.java

--- src/main/java/com/example/domaserver/domain/rank/service/impl/UpdateRankingServiceImpl.java
+++ src/main/java/com/example/domaserver/domain/rank/service/impl/UpdateRankingServiceImpl.java
@@ -2,21 +2,18 @@ package com.example.domaserver.domain.rank.service.impl;
 
 import com.example.domaserver.domain.rank.entity.Rank;
 import com.example.domaserver.domain.rank.service.UpdateRankingService;
-import com.example.domaserver.global.annotation.ServiceWithReadOnlyTransactional;
 import com.example.domaserver.global.annotation.ServiceWithTransaction;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 
 
 @ServiceWithTransaction
+@RequiredArgsConstructor
 public class UpdateRankingServiceImpl implements UpdateRankingService {
 
     private static final String UPDATE_RANKING = "update ranking set rank=rank+1 where rank=?";
 
     private final RedisTemplate redisTemplate;
-
-    public UpdateRankingServiceImpl(RedisTemplate redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
 
     public void updateRanking(Rank rank) {
         redisTemplate.opsForSet().add(UPDATE_RANKING, rank.getUser().getId(), rank.getPenaltyPoints());
