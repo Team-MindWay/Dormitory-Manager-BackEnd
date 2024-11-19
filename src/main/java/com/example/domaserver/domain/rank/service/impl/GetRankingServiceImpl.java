7e202beaf5680fe04e2fe8a7909d3253a4144cd2
김한주 <s24043@gsm.hs.kr>
2024-11-19 20:00:53 +0900

    modify :: GetRankingServiceImpl.java

--- src/main/java/com/example/domaserver/domain/rank/service/impl/GetRankingServiceImpl.java
+++ src/main/java/com/example/domaserver/domain/rank/service/impl/GetRankingServiceImpl.java
@@ -3,21 +3,16 @@ package com.example.domaserver.domain.rank.service.impl;
 import com.example.domaserver.domain.rank.service.GetRankingService;
 import com.example.domaserver.domain.user.entity.User;
 import com.example.domaserver.global.annotation.ServiceWithReadOnlyTransactional;
-import com.example.domaserver.global.annotation.ServiceWithTransaction;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 
-
-@ServiceWithTransaction
 @ServiceWithReadOnlyTransactional
+@RequiredArgsConstructor
 public class GetRankingServiceImpl implements GetRankingService {
 
     private static final String GET_RANKING = "getRank";
 
     private final RedisTemplate redisTemplate;
-
-    public GetRankingServiceImpl(RedisTemplate redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
 
     public Long getRanking(User user) {
         Long rank = redisTemplate.opsForZSet().rank(GET_RANKING, user.getId());
