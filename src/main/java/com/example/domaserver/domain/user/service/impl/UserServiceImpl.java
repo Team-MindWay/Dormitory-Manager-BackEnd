39b309517119978a7f3fcf21d2a65cfc2a2e7388
김한주 <s24043@gsm.hs.kr>
2025-04-03 20:30:43 +0900

    modify :: UserServiceImpl.java

--- src/main/java/com/example/domaserver/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/domaserver/domain/user/service/impl/UserServiceImpl.java
@@ -2,11 +2,12 @@ package com.example.domaserver.domain.user.service.impl;
 
 import com.example.domaserver.domain.user.entity.User;
 import com.example.domaserver.domain.user.exception.NotFoundException;
+import com.example.domaserver.domain.user.exception.UserNotFoundException;
+import com.example.domaserver.domain.user.exception.UsernameNotFoundException;
 import com.example.domaserver.domain.user.repository.UserRepository;
 import com.example.domaserver.domain.user.service.UserService;
 import com.example.domaserver.global.annotation.ServiceWithTransaction;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.UUID;
 
@@ -19,13 +20,12 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findByUsername(String name) {
         return userRepository.findByUsername(name)
-                .orElseThrow(() -> new NotFoundException("User not found with username: " + name));
+                .orElseThrow(() -> new NotFoundException("User not found with username " + name));
     }
 
     @Override
     public User findById(UUID id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("User not found with id: " + id));
+                .orElseThrow(() -> new NotFoundException("User not found with id " + id));
     }
-
 }
